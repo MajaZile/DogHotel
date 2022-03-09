4778c783f099508a4b5df37c9384f8ba06024df7
MajaZile <zivkovic.maja777@gmail.com>
2022-03-09 15:14:41 +0100

    Removed jquery

--- HotelZaPse/HotelZaPse/App_Start/BundleConfig.cs
+++ HotelZaPse/HotelZaPse/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace HotelZaPse
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.4.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                          "~/Scripts/jquery.validate*", "~/Scripts/jquery.validate.unobtrusive.min.js",

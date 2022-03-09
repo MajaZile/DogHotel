55173218eb26c4e5536f873229c8c9e4a3446ba6
MajaZile <zivkovic.maja777@gmail.com>
2022-03-09 14:51:42 +0100

    Removed jquery.validate and jquery.obtrusive.validate packages

--- HotelZaPse/HotelZaPse/App_Start/BundleConfig.cs
+++ HotelZaPse/HotelZaPse/App_Start/BundleConfig.cs
@@ -11,19 +11,19 @@ namespace HotelZaPse
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                         "~/Scripts/jquery.validate*", "~/Scripts/jquery.validate.unobtrusive.min.js",
+                         "~/Scripts/jquery.validate.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css", "~/Content/style.css", "~/Content/all.min.css"));
         }
     }
 }
